5aa3e9b8f49ae7d64004f3dbb5d97782589d046d
Nicole Ferreira <nefesa502@gmail.com>
2021-12-05 18:52:08 -0300

    adicionada nova aula3

---
+++ aula03/exemplos/Exemplo01.java
@@ -0,0 +1,28 @@
+package exemplos;
+
+/**
+ * Este é o primeiro exemplo
+ * autor: Nicole
+ * data: 01/12/2021
+ */
+
+ /* abrir o comentário
+
+    fechar o comentário
+ */
+
+ // comentário de linda
+// syso = atalho para System.out.print
+
+public class Exemplo01 {
+
+    public static void main(String[] args) {
+        System.out.println("Funcionou!"); // "" = texto
+        System.out.println("1 + 2");
+        System.out.println(1 + 3); //operação aritmética
+        System.out.println(2 + 3 *5);
+        System.out.println((2 + 3) *5);
+
+    }
+
+}
