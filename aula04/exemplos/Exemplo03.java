67cb7c36d3e3bc16cfbe96c4ac4f330d858a1c78
Nicole Ferreira <nefesa502@gmail.com>
2021-12-05 22:41:53 -0300

    aula 04 - parte 3

--- aula04/exemplos/Exemplo03.java
+++ aula04/exemplos/Exemplo03.java
@@ -7,9 +7,10 @@ public class Exemplo03 {
         n1 = 10;
         n2 = 15;
 
-        System.out.println( n1 < n2 );
-        System.out.println( n1 > n2 );
-        System.out.println( n1 != n2 );
-        System.out.println( n1 == n2 );
+        System.out.println(n1 < n2);
+        System.out.println(n1 > n2);
+        System.out.println(n1 != n2);
+        System.out.println(n1 == n2);
+        
     }
 }
