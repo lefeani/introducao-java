2b7c27c5bbca77e806c473562667a182a84d8997
Nicole Ferreira <nefesa502@gmail.com>
2021-12-06 03:35:06 -0300

    aula 05 - parte 2

---
+++ aula05/exercicios/Exercicio04.java
@@ -0,0 +1,24 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int numero = 1, soma;
+
+        soma = 0;
+        for (int cont = 1 ; numero != 0 ; cont++) {
+            System.out.println("Digite o " + cont + "º número:");
+            numero = entrada.nextInt();
+
+            soma = soma + numero;     
+        }
+
+        System.out.println("Soma = " + soma);
+
+        entrada.close();
+    }
+
+
+}
